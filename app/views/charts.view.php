076c2f70f00ab0f4327409da038d45e92040be58
Omar Ouahoud <o.ouahoud@sapalomera.cat>
2025-11-30 21:49:46 +0100

    Add button to create fragrance in navbar

--- app/views/charts.view.php
+++ app/views/charts.view.php
@@ -34,6 +34,11 @@
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
+        <a class="create-fragrance-btn" href="../../index.php?action=fragrance" title="Create New Fragrance">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </a>
       </div>
     <?php endif; ?>
   </div>
@@ -117,7 +122,10 @@
         <div class="card">
           <div class="card-image-container">
             <img src="<?= htmlspecialchars($fragrance['image_url']) ?>" alt="<?= htmlspecialchars($fragrance['name']) ?>">
-            <div class="favorite-icon"></div>
+            <?php if (isLoggedIn()): ?>
+            <div class="favorite-icon"></div>            
+              <a class="edit-pill" href="../../index.php?action=fragrance&id=<?= urlencode($fragrance['id']) ?>">Edit</a>
+            <?php endif; ?>
           </div>
           <p><?= htmlspecialchars($fragrance['name']) ?></p>
         </div>
