1ccd739aaefb167f0dee67d56212fd2cd228c5b4
Omar Ouahoud <o.ouahoud@sapalomera.cat>
2025-12-09 23:49:26 +0100

    Added a rickroll wikipedia link as a placeholder because Wave doesn't like empty links (the features are not yet implemented smh)

--- app/views/charts.view.php
+++ app/views/charts.view.php
@@ -24,7 +24,7 @@
       </div>
     <?php else: ?>
       <div id="icons-container">
-        <a href="#">
+        <a href="https://en.wikipedia.org/wiki/Never_Gonna_Give_You_Up" title="Favorites">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
           </svg>
@@ -34,7 +34,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
           </svg>
         </a>
-        <a href="#">
+        <a href="https://en.wikipedia.org/wiki/Never_Gonna_Give_You_Up" title="Profile">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
           </svg>
